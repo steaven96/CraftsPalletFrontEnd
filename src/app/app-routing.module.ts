f1dd5575a2799471359f52a89965e20f32b69293
steavenx <steavenx.gunturu@intel.com>
2022-05-11 00:06:24 +0530

    added route for craftspallet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'craftsPalletFrontEnd', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./container/home/home.module').then(mod => mod.HomeModule) },
   { path: 'admin', loadChildren: () => import('./container/admin/admin.module').then(mod => mod.AdminModule) },
   { path: 'aboutus', loadChildren: () => import('./container/aboutus/aboutus.module').then(mod => mod.AboutusModule) },
