867e2f15c38d81ca8afca17132ebdf1fd331e74a
steavenx <steavenx.gunturu@intel.com>
2022-05-11 00:11:03 +0530

    route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'CraftsPalletFrontEnd/craftsPalletFrontEnd', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'CraftsPalletFrontEnd', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./container/home/home.module').then(mod => mod.HomeModule) },
   { path: 'admin', loadChildren: () => import('./container/admin/admin.module').then(mod => mod.AdminModule) },
   { path: 'aboutus', loadChildren: () => import('./container/aboutus/aboutus.module').then(mod => mod.AboutusModule) },
